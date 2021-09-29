6522dfc5c9a32d3f21aa34624d28de07a1402b46
m-b-t-n <mbtn_0xff@outlook.com>
2021-09-29 19:13:40 +0900

    ranger: Switch the name of "ranger" according to the OS environment
     Switch the name of "ranger".
     * Windows:      `ranger.py`
     * macOS, Linux: `ranger`
    
     refs #44

--- common/ranger/ranger.sh
+++ common/ranger/ranger.sh
@@ -1,8 +1,19 @@
 #!/bin/bash
 
+readonly UNAME="$(uname)"
+
+if echo "${UNAME}" | grep -i ^mingw.* >/dev/null; then
+	RANGER_EXEC_CMD="ranger.py"
+
+elif [[ "${UNAME}" = "Linux" ]] || [[ "${UNAME}" = "Darwin" ]]; then
+	RANGER_EXEC_CMD="ranger"
+else
+	RANGER_EXEC_CMD="ranger"
+fi
+
 function _ranger() {
 	if [[ -z "${RANGER_LEVEL}" ]]; then
-		ranger
+		${RANGER_EXEC_CMD}
 	else
 		exit
 	fi
