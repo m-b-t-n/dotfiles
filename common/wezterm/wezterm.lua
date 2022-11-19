0cb8c0b60e04d0b7ca1db63287c0afc2aeeea56d
m-b-t-n <mbtn_0xff@outlook.com>
2022-11-20 04:55:31 +0900

    wezterm: Set transparency both window and text area
     Following as;
     [Window Background Opacity](https://wezfurlong.org/wezterm/config/appearance.html#window-background-opacity)
    
     refs #79

--- common/wezterm/wezterm.lua
+++ common/wezterm/wezterm.lua
@@ -4,5 +4,7 @@ return {
 	font = wezterm.font("Ricty Diminished Discord", {weight="Regular", stretch="Normal", style="Normal"}),
 	font_size = 20.0,
 	color_scheme = "Pastel White (terminal.sexy)",
+	window_background_opacity = 0.7,
+	text_background_opacity = 0.7,
 }
 
