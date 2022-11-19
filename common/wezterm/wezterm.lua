ef050c2ba8b5e450deb1dfd4f1275590800d18dc
m-b-t-n <mbtn_0xff@outlook.com>
2022-11-20 04:41:47 +0900

    wezterm: Set colorscheme
     [Pastel White (terminal.sexy)](https://wezfurlong.org/wezterm/colorschemes/p/index.html#pastel-white-terminalsexy)
    
     refs #79

--- common/wezterm/wezterm.lua
+++ common/wezterm/wezterm.lua
@@ -3,5 +3,6 @@ local wezterm = require 'wezterm'
 return {
 	font = wezterm.font("Ricty Diminished Discord", {weight="Regular", stretch="Normal", style="Normal"}),
 	font_size = 20.0,
+	color_scheme = "Pastel White (terminal.sexy)",
 }
 
