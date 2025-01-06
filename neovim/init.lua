d72964556c45797015d6d7c1b1071299230cdc35
m-b-t-n <mbtn_0xff@outlook.com>
2025-01-06 10:58:25 +0900

    neovim: Import global settings from vimrc
     refs #206

---
+++ neovim/init.lua
@@ -0,0 +1,32 @@
+-- Global Settings
+vim.colorscheme = 'desert'
+vim.opt.title = true
+vim.opt.number = true
+vim.opt.tabstop = 8
+vim.opt.autoindent = true
+vim.opt.smartindent = true
+vim.opt.showmatch = true
+vim.opt.matchtime = 1
+-- vim.opt.t_Co = 256
+vim.opt.clipboard = 'unnamed,unnamedplus'
+vim.opt.list = true
+vim.opt.listchars = 'tab:> ,trail:_,nbsp:%,extends:>,precedes:<'
+vim.opt.cursorline = true
+vim.opt.laststatus = 2
+vim.opt.background = 'dark'
+vim.opt.incsearch = true
+vim.opt.ruler = true
+vim.opt.syntax = 'on'
+vim.opt.hidden = true
+vim.opt.writebackup = false
+vim.opt.undofile = false
+vim.opt.backup = false
+vim.opt.cmdheight = 2
+vim.opt.fileformats = 'unix,dos,mac'
+vim.opt.fileencodings = 'utf-8,sjis,cp932,euc-jp'
+vim.opt.wildmenu = true
+vim.opt.signcolumn = 'yes'
+vim.opt.ambiwidth = 'double'
+vim.opt.nrformats = 'unsigned,bin,hex'
+vim.cmd.colorscheme('desert')
+
