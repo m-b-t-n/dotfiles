dfe745ec96ae6021315b8cada0d6b5703b6d38d1
m-b-t-n <mbtn_0xff@outlook.com>
2025-01-07 13:57:27 +0900

    neovim: Move global settings to the top of init.lua
     refs #206

--- neovim/init.lua
+++ neovim/init.lua
@@ -1,3 +1,36 @@
+-- Global Settings
+vim.colorscheme = 'desert'
+vim.opt.title = true
+vim.opt.number = true
+vim.opt.tabstop = 8
+vim.opt.autoindent = true
+vim.opt.smartindent = true
+vim.opt.showmatch = true
+vim.opt.matchtime = 1
+-- vim.opt.t_Co = 256
+vim.opt.clipboard = 'unnamed,unnamedplus'
+vim.opt.list = true
+vim.opt.listchars = 'tab:> ,trail:_,nbsp:%,extends:>,precedes:<'
+vim.opt.cursorline = true
+vim.opt.laststatus = 2
+vim.opt.background = 'dark'
+vim.opt.incsearch = true
+vim.opt.ruler = true
+vim.opt.syntax = 'on'
+vim.opt.hidden = true
+vim.opt.writebackup = false
+vim.opt.undofile = false
+vim.opt.backup = false
+vim.opt.cmdheight = 2
+vim.opt.fileformats = 'unix,dos,mac'
+vim.opt.fileencodings = 'utf-8,sjis,cp932,euc-jp'
+vim.opt.wildmenu = true
+vim.opt.signcolumn = 'yes'
+vim.opt.ambiwidth = 'double'
+vim.opt.nrformats = 'unsigned,bin,hex'
+vim.cmd.colorscheme('desert')
+vim.opt.helplang = 'ja,en'
+
 -- Bootstrap lazy.nvim
 local lazypath = vim.fn.stdpath("data") .. "/lazy/lazy.nvim"
 if not (vim.uv or vim.loop).fs_stat(lazypath) then
@@ -40,37 +73,4 @@ require("lazy").setup({
   -- automatically check for plugin updates
   checker = { enabled = true },
 })
-
--- Global Settings
-vim.colorscheme = 'desert'
-vim.opt.title = true
-vim.opt.number = true
-vim.opt.tabstop = 8
-vim.opt.autoindent = true
-vim.opt.smartindent = true
-vim.opt.showmatch = true
-vim.opt.matchtime = 1
--- vim.opt.t_Co = 256
-vim.opt.clipboard = 'unnamed,unnamedplus'
-vim.opt.list = true
-vim.opt.listchars = 'tab:> ,trail:_,nbsp:%,extends:>,precedes:<'
-vim.opt.cursorline = true
-vim.opt.laststatus = 2
-vim.opt.background = 'dark'
-vim.opt.incsearch = true
-vim.opt.ruler = true
-vim.opt.syntax = 'on'
-vim.opt.hidden = true
-vim.opt.writebackup = false
-vim.opt.undofile = false
-vim.opt.backup = false
-vim.opt.cmdheight = 2
-vim.opt.fileformats = 'unix,dos,mac'
-vim.opt.fileencodings = 'utf-8,sjis,cp932,euc-jp'
-vim.opt.wildmenu = true
-vim.opt.signcolumn = 'yes'
-vim.opt.ambiwidth = 'double'
-vim.opt.nrformats = 'unsigned,bin,hex'
-vim.cmd.colorscheme('desert')
-vim.opt.helplang = 'ja,en'
 
