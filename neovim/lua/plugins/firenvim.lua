b3040ae98e8996e08b067c23ecd0180b68041d28
m-b-t-n <mbtn_0xff@outlook.com>
2025-03-19 22:12:56 +0900

    firenvim: Enable only "github.com"
     Set Default to disable because there are some unexpected behavior
     on several sites.
    
     refs #218

--- neovim/lua/plugins/firenvim.lua
+++ neovim/lua/plugins/firenvim.lua
@@ -4,6 +4,21 @@ return {
     {
         "glacambre/firenvim",
         build = ":call firenvim#install(0)",
+        config = function()
+            vim.g.firenvim_config = {
+                globalSettings = {},
+                localSettings = {
+                    [".*"] = {
+                        selector = "",
+                        priority = 0,
+                    },
+                    ["github.com"] = {
+                        selector = "textarea",
+                        priority = 1,
+                    },
+                },
+            }
+        end,
     },
 }
 
