109168a7208962eb6cc1fe5def6da0866bbaff75
m-b-t-n <mbtn_0xff@outlook.com>
2026-01-07 09:45:06 +0900

    neovim: Add mcphub.nvim
     https://github.com/ravitemer/mcphub.nvim
    
     refs #280

---
+++ neovim/lua/plugins/mcphub-nvim.lua
@@ -0,0 +1,15 @@
+-- https://github.com/ravitemer/mcphub.nvim
+
+return {
+    {
+        'ravitemer/mcphub.nvim',
+        dependencies = {
+            "nvim-lua/plenary.nvim",
+        },
+        build = "npm install -g mcp-hub@latest",
+        config = function()
+            require("mcphub").setup()
+        end,
+    },
+}
+
