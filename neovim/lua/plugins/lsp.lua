d778a6956f61706084ccc79e47ee1e804c1c932a
m-b-t-n <mbtn_0xff@outlook.com>
2026-01-07 16:32:22 +0900

    neovim: Check whether fname is number or not
     Normally, `fname` is a string, but in some case, it isn't.
    
     refs #283

--- neovim/lua/plugins/lsp.lua
+++ neovim/lua/plugins/lsp.lua
@@ -27,6 +27,9 @@ return {
                 vim.lsp.config(lsp_server, {
                     root_dir = function(fname)
                         local lspconfig_util = require("lspconfig.util")
+                        if type(fname) == "number" then
+                            fname = vim.api.nvim_buf_get_name(fname)
+                        end
                         return lspconfig_util.find_git_ancestor(fname) or vim.fn.getcwd()
                     end,
                 })
