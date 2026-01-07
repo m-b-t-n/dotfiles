5c10944b6ebc1ea737df9328a6ecfa851f3396d4
m-b-t-n <mbtn_0xff@outlook.com>
2026-01-07 13:01:02 +0900

    neovim: Collaborate codecompanion and MCPHub
     refs #280

--- neovim/lua/plugins/codecompanion-nvim.lua
+++ neovim/lua/plugins/codecompanion-nvim.lua
@@ -6,15 +6,45 @@ return {
         dependencies = {
             "nvim-lua/plenary.nvim",
             "nvim-treesitter/nvim-treesitter",
+            "zbirenbaum/copilot.lua",
+            "ravitemer/mcphub.nvim",
         },
         opts = {
             adapters = {
             },
             strategies = {
+                chat = {
+                    tools = {
+                        ["mcp"] = {
+                            -- 必要時に拡張をロードする（mcphub Wiki の推奨パターン）
+                            callback = function()
+                                return require("mcphub.extensions.codecompanion")
+                            end,
+                            description = "Call tools and resources from the MCP Servers",
+                        },
+                    },
+                },
             },
             language = {
                 "Japanese",
             },
+            extensions = {
+                mcphub = {
+                    callback = "mcphub.extensions.codecompanion",
+                    opts = {
+                        -- MCP tools
+                        make_tools = true,
+                        show_server_tools_in_chat = true,
+                        add_mcp_prefix_to_tool_names = false,
+                        show_result_in_chat = true,
+                        format_tool = nil,
+                        -- MCP resources
+                        make_vars = true,
+                        -- MCP prompts
+                        make_slash_commands = true,
+                    },
+                },
+            }
         },
     },
 }
