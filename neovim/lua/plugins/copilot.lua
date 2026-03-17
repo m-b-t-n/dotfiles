1a430e22efabdf8474284a85bc8ac2aff6f540c4
m-b-t-n <mbtn_0xff@outlook.com>
2026-03-17 18:04:27 +0900

    neovim: Set key bindings for copilot.lua
     To avoid duplicated bindings for another plugins.
    
     refs #293

--- neovim/lua/plugins/copilot.lua
+++ neovim/lua/plugins/copilot.lua
@@ -8,7 +8,10 @@ return {
             suggestion = {
                 auto_trigger = true,
                 keymap = {
-                    accept = "<Tab>",
+                    accept = "<M-l>",
+                    next = "<M-]>",
+                    prev = "<M-[>",
+                    dismiss = "<C-]>",
                 },
             },
         },
